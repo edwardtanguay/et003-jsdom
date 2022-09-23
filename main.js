86f80f4ad746872f74cbf1b61810660b40b71b34
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-23 15:05:39 +0200

    1 and 2 ready

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const messageElem = document.querySelector('.message');
+const message1Elem = document.querySelector('.message1');
+const message2Elem = document.querySelector('.message2');
 const userText1Elem = document.querySelector('.userText1');
 const userText2Elem = document.querySelector('.userText2');
 const changeText1ButtonElem = document.querySelector('.changeText1Button');
@@ -7,12 +8,11 @@ const changeText2ButtonElem = document.querySelector('.changeText2Button');
 userText1Elem.focus();
 
 const changeTheText = () => {
-	messageElem.innerText = userText1Elem.value;
+	message1Elem.innerText = userText1Elem.value;
 	userText1Elem.value = '';
 	userText1Elem.focus();
 };
 
-messageElem.innerText = 'initial text';
 changeText1ButtonElem.addEventListener('click', changeTheText);
 
 userText1Elem.addEventListener('keyup', (e) => {
