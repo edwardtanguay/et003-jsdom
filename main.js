77d990e3575874cf2526ea77e61589dd349902a4
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-23 15:00:03 +0200

    input works

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const messageElem = document.querySelector('.message');
+const userTextElem = document.querySelector('.userText');
 const changeTextButtonElem = document.querySelector('.changeTextButton');
 
+userTextElem.focus();
+
+const changeTheText = () => {
+	messageElem.innerText = userTextElem.value;
+	userTextElem.value = '';
+	userTextElem.focus();
+};
+
 messageElem.innerText = 'initial text';
-changeTextButtonElem.addEventListener('click', () => {
-	messageElem.innerText = 'the button was clicked';
+changeTextButtonElem.addEventListener('click', changeTheText);
+
+userTextElem.addEventListener('keyup', (e) => {
+	if (e.keyCode === 13) {
+		changeTheText();
+	}
 });
